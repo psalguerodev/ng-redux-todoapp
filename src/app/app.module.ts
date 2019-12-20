d923c1267142307f2a9dbd0a779125b9c9273553
Patrick Salguero <patrick.salguero.avalos@gmail.com>
2019-12-20 13:30:47 -0500

    Was add structure ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoAddComponent } from './todo/todo-add/todo-add.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoItemComponent } from './todo/todo-item/todo-item.component';
+import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoHeaderComponent } from './todo/todo-header/todo-header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoAddComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    TodoFooterComponent,
+    TodoComponent,
+    TodoHeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
